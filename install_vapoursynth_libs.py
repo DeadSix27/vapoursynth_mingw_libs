7b46a94ef7f9e5d7f4f09722d420212877732dfe
DeadSix27 <DeadSix27@users.noreply.github.com>
2021-08-13 00:08:16 +0200

    Update install_vapoursynth_libs.py

--- install_vapoursynth_libs.py
+++ install_vapoursynth_libs.py
@@ -101,7 +101,10 @@ else:
 		runCmd("cp {0} ../bin".format("VapourSynth.dll"))
 		
 		pydName = "vapoursynth.cp36-win_amd64.pyd"
-		if float(ver_suff) >= 49: # Version 49 and above use python 3.8
+		if float(ver_suff) >= 53: # Version 53 and above use python 3.9
+			pydName = "vapoursynth.cp39-win_amd64.pyd"
+			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.9").replace("%%PY_VER%%","39")
+		elif float(ver_suff) >= 49: # Version 49 and above use python 3.8
 			pydName = "vapoursynth.cp38-win_amd64.pyd"
 			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.8").replace("%%PY_VER%%","38")
 		elif float(ver_suff) > 44: # Version 45 and above use python 3.7		
