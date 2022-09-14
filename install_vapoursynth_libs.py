a472959e0bf5f212695e4ce4aec63952c8a2f8a6
DeadSix27 <DeadSix27@users.noreply.github.com>
2022-09-14 02:22:22 +0200

    add R59

--- install_vapoursynth_libs.py
+++ install_vapoursynth_libs.py
@@ -101,6 +101,9 @@ else:
 		runCmd("cp {0} ../bin".format("VapourSynth.dll"))
 		
 		pydName = "vapoursynth.cp36-win_amd64.pyd"
+		if float(ver_suff) >= 58: # Version 53 and above use python 3.9
+			pydName = "vapoursynth.cp310-win_amd64.pyd"
+			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.10").replace("%%PY_VER%%","310")
 		if float(ver_suff) >= 53: # Version 53 and above use python 3.9
 			pydName = "vapoursynth.cp39-win_amd64.pyd"
 			VSS_PC = VSS_PC.replace("%%PY_VER_DOT%%","3.9").replace("%%PY_VER%%","39")
